ab1d7857b7e611e0d8184b0c8c7e80edac2775be
ry-blank <52143833+ry-blank@users.noreply.github.com>
2019-10-29 16:06:45 -0500

    Unit Tests for a Class Assignment
    5th commit - Unit test, test_object_not_created_error_last_name(self), now passing

--- student_class.py
+++ student_class.py
@@ -12,6 +12,9 @@ class Student:
     """Student class"""
 
     def __init__(self, lname, fname, major, gpa=0.0):
+        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
+        if not (name_characters.issuperset(lname)):
+            raise ValueError
         self.last_name = lname
         self.first_name = fname
         self.major = major
@@ -19,4 +22,4 @@ class Student:
 
     def __str__(self):
         return self.last_name + ", " + self.first_name + \
-               " has major " + self.major + "with gpa: " + str(self.gpa)
+               " has major " + self.major + " with gpa: " + str(self.gpa)
